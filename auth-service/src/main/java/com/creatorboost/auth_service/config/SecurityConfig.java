a527ac68eb923143de5a39097e72cc2cba2472b1
Chamindu24 <chamindus.22@cse.mrt.ac.lk>
2025-07-31 17:09:43 +0530

    Add initial implementation of notification service with Kafka integration and CORS configuration

--- auth-service/src/main/java/com/creatorboost/auth_service/config/SecurityConfig.java
+++ auth-service/src/main/java/com/creatorboost/auth_service/config/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig {
         http.cors(Customizer.withDefaults())
             .csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/login","/register","/send-reset-otp","/reset-password","/logout").permitAll()
+                .requestMatchers("/login","/register","/send-reset-otp","/reset-password","/logout","/test/**").permitAll()
                 .anyRequest().authenticated())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .logout(AbstractHttpConfigurer::disable)
