4efd22471c39728d132c0a718ed1a78ffe992ae4
Chamindu Sathsara <166353817+Chamindu24@users.noreply.github.com>
2025-08-10 09:14:41 +0530

    Delete gig_service directory

--- gig_service/src/main/com/example/gig_service/repository/GigImageRepository.java
+++
@@ -1,8 +0,0 @@
-package src.main.com.example.gig_service.repository;
-
-import src.main.com.example.gig_service.entity.GigImage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface GigImageRepository extends JpaRepository<GigImage, UUID> {}
--- gig_service/src/main/com/example/gig_service/repository/GigPackageRepository.java
+++
@@ -1,8 +0,0 @@
-package src.main.com.example.gig_service.repository;
-
-import src.main.com.example.gig_service.entity.GigPackage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface GigPackageRepository extends JpaRepository<GigPackage, UUID> {}
