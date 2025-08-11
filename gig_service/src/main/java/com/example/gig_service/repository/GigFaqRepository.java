106367fd7ac3f107dc50e72d65ced426d8236c95
Chamindu Sathsara <166353817+Chamindu24@users.noreply.github.com>
2025-08-11 14:06:54 +0530

    Delete gig_service directory

--- gig_service/src/main/java/com/example/gig_service/repository/GigFaqRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.gig_service.repository;
-
-import com.example.gig_service.entity.GigFaq;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface GigFaqRepository extends JpaRepository<GigFaq, UUID> {}
