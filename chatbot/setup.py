169898679cddcc35f2a6dccaa33a6b891282295c
Chamindu24 <chamindus.22@cse.mrt.ac.lk>
2025-09-04 08:08:55 +0530

    Add initial project structure and setup files for CreatorBoost ChatBot

---
+++ chatbot/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'CreatorBoost ChatBot',
+    version= '0.0.0',
+    author= 'Chamindu Sathsara',
+    author_email= 'chamindusathsara27@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
