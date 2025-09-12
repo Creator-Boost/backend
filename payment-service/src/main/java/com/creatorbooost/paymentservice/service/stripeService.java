398508735fef39e365f6d6e9b6e09b239c1be5d4
isukasandiw <isukasandiw@gmail.com>
2025-09-13 00:15:51 +0530

    payment-service

---
+++ payment-service/src/main/java/com/creatorbooost/paymentservice/service/stripeService.java
@@ -0,0 +1,54 @@
+package com.creatorbooost.paymentservice.service;
+
+import com.creatorbooost.paymentservice.dto.ProductRequest;
+import com.creatorbooost.paymentservice.dto.StripeResponse;
+
+
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.checkout.Session;
+import com.stripe.param.checkout.SessionCreateParams;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+@Service
+public class stripeService {
+    @Value("${stripe.secretKey}")
+    private String secretKey;
+    //stripe-API
+    //->productName,amount ,quantity , currency
+    //-> return sessionId and url
+    public StripeResponse checkoutProducts(ProductRequest productRequest){
+        Stripe.apiKey=secretKey;
+
+        SessionCreateParams.LineItem.PriceData.ProductData productData = SessionCreateParams.LineItem.PriceData.ProductData.builder()
+                .setName(productRequest.getName()).build();
+        SessionCreateParams.LineItem.PriceData priceData = SessionCreateParams.LineItem.PriceData.builder()
+                .setCurrency(productRequest.getCurrency() == null ? "USD" : productRequest.getCurrency())
+                .setUnitAmount(productRequest.getAmount())
+                .setProductData(productData)
+                .build();
+        SessionCreateParams.LineItem lineItem = SessionCreateParams.LineItem.builder()
+                .setQuantity(productRequest.getQuantity())
+                .setPriceData(priceData)
+                .build();
+        SessionCreateParams params = SessionCreateParams.builder()
+                .setMode(SessionCreateParams.Mode.PAYMENT)
+                .setSuccessUrl("http://localhost:8081/success")
+                .setCancelUrl("http://localhost:8081/cancel")
+                .addLineItem(lineItem)
+                .build();
+        Session session=null;
+        try {
+            session=Session.create(params);
+        }catch (StripeException ex){
+            //log
+            System.out.println(ex.getMessage());
+        }
+        return StripeResponse.builder()
+                .status("success")
+                .message(" payment success created")
+                .sessionId(session.getId())
+                .sessionUrl(session.getUrl())
+                .build();
+    }
+}
---
+++ payment-service/src/test/java/com/creatorbooost/paymentservice/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creatorbooost.paymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
