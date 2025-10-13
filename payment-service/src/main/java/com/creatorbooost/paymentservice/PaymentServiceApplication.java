dabeb9da6a9bb6a7640acc8787590f6dcb65a07a
Ravindu Sandeep <sandeepravindu112@gmail.com>
2025-10-13 23:39:45 +0530

    tp payement commit

--- payment-service/src/main/java/com/creatorbooost/paymentservice/PaymentServiceApplication.java
+++ payment-service/src/main/java/com/creatorbooost/paymentservice/PaymentServiceApplication.java
@@ -2,8 +2,10 @@ package com.creatorbooost.paymentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.creatorbooost.paymentservice")
 public class PaymentServiceApplication {
 
     public static void main(String[] args) {
